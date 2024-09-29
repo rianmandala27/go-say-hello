f36d0568bf7e77b70ef485218babd5063455f5d5
rian <rianmandala959@gmail.com>
2024-09-29 18:45:12 +0700

    update name hello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rianmandala27/go-say-hello
+module github.com/rianmandala27/go-say-hello/v2
 
 go 1.23.1
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello word master"
+func SayHello(name string) string {
+	return "Hello word master " + name
 }
